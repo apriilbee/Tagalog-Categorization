ce43f07bdd9a1bc74f550bc1b448125b33854ea1
aprildaebation <adaecb@gmail.com>
2017-03-04 00:14:19 +0800

    python code for shuffling data

---
+++ feature_extraction/shuffle_documents.py
@@ -0,0 +1,10 @@
+import random
+
+with open('../data/stemmed_.csv') as f:
+    data = f.readlines()
+
+header, rest = data[0], data[1:]
+random.shuffle(rest)
+
+with open('../data/shuffled_stemmed.csv', 'w') as out:
+    out.write(''.join([header]+rest))
