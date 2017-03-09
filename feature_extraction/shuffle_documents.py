11d582d37f7bb9c8f769a9cecbbcbc7fd84aa9d1
aprildaebation <adaecb@gmail.com>
2017-03-10 01:15:49 +0800

    compared predicted with actual labels

--- feature_extraction/shuffle_documents.py
+++ feature_extraction/shuffle_documents.py
@@ -6,5 +6,5 @@ with open('../data/stemmed_.csv') as f:
 header, rest = data[0], data[1:]
 random.shuffle(rest)
 
-with open('../data/shuffled_stemmed.csv', 'w') as out:
+with open('../data/shuffled.csv', 'w') as out:
     out.write(''.join([header]+rest))
