786976d100226291d081d490e9d44d9279d57eb2
aprildaebation <adaecb@gmail.com>
2017-06-09 00:16:17 +0800

    updated preprocessing files

--- building classifier/shuffle_documents.py
+++ building classifier/shuffle_documents.py
@@ -1,10 +1,13 @@
 import random
 
-with open('../data/no_stopwords.csv') as f:
+# with open('../data/no_stopwords.csv') as f:
+#     data = f.readlines()
+
+with open('../data/experiment1/stemmed_.csv') as f:
     data = f.readlines()
 
 header, rest = data[0], data[1:]
 random.shuffle(rest)
 
-with open('../data/no_stemming.csv', 'w') as out:
+with open('../data/experiment1/shuffled_updated.csv', 'w') as out:
     out.write(''.join([header]+rest))
